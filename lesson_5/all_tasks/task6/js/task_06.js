6aa72d472070b652c83fb7f43c144bf4bfb8aae9
maripunchik <162891408+maripunchik@users.noreply.github.com>
2025-03-02 12:23:40 +0100

    Add files via upload

---
+++ lesson_5/all_tasks/task6/js/task_06.js
@@ -0,0 +1,17 @@
+// cуворий режим
+"use strict"
+
+// ==========================================================
+
+// 3. Вивести таблицю з одним рядком і  7 стовпцями.
+
+ if (confirm("Почати тестування?")) {
+  document.write(`<table class="task__table">`);
+    document.write(`<tr>`);
+    for (let i = 1; i <= 7; i++) {
+      document.write(`<td>${i}</td>`);
+    }
+    document.write(`</tr>`);
+  document.write(`</table>`);
+	}
+  
